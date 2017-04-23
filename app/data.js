e73c46706c27ce8d04e514b8b3bdfae5b69749d2
wangjishu <wangjishu8@sina.com>
2017-04-23 19:33:26 +0800

    Create data.js

---
+++ app/data.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+var username = [{
+    id : 1,
+    name : 'Adam Noah Levine',
+    work : 'UI',
+    img :"images/a4.jpg",
+},{
+	id : 2,
+    name : 'James Burgon Valentine',
+    work : '框架',
+    img :"images/a7.jpg",
+},{
+    id : 3,
+    name : 'Jesse Royal Carmichael',
+    work : '美工',
+    img :"images/a9.jpg",
+},{
+    id : 4,
+    name : 'Mickey "Mickey" Madden',
+    work : '项目经理',
+    img :"images/a3.jpg",
+}];
